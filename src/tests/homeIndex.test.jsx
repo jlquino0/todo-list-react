ab96056b14aa48d074e59fb48621e053cea05572
BANTRAB\jorge_quinonez <jorge_quinonez@bantrab.com>
2023-10-22 23:33:09 -0600

    mock custom hook resuelto

--- src/tests/homeIndex.test.jsx
+++ src/tests/homeIndex.test.jsx
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react'
 import { Home } from '../pages/Home/index'
-import { useTodos } from '../pages/hooks/useTodos'
+//import { useTodos } from '../pages/hooks/useTodos'
+import * as useTodosM from '../pages/hooks/useTodos'
 
 const mockUsedNavigate = jest.fn();
 jest.mock('react-router-dom', () => ({
@@ -14,11 +15,7 @@ jest.mock('react-router-dom', () => ({
 //  default: (...args) => mockuseTodos(...args),
 //}))
 
-const mockuseTodos = jest.fn()
-jest.mock('../pages/hooks/useTodos', () => ({
-  __esModule: true,
-  default: (...args) => mockuseTodos(...args),
-}))
+useTodosM.useTodos =  jest.fn();
 
 describe('Pruebas en home index', () => {
 
